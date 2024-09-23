2ede334036bb3b3ae7f68c3b00ec3297eb11d8ec
Alexandr Kabanau <azraill1989@gmail.com>
2024-09-23 16:22:25 +0300

    add: add tabs into back structure

--- app/API/users/route.ts
+++ app/API/users/route.ts
@@ -1,9 +1,19 @@
 import { prisma } from '@/prisma/PrismaClient';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   const users = await prisma.user.findMany();
-  console.log(users);
+  // console.log(users);
 
   return NextResponse.json(users);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+
+  const user = await prisma.user.create({
+    data,
+  });
+
+  return user;
 }
