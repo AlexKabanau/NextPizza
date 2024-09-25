c647976707362cea1a52c665179e932b07c13c17
Alexandr Kabanau <Azraill1989@gmail.com>
2024-09-25 20:30:48 +0300

    fix: fix bug

--- app/API/products/search/route.ts
+++ app/API/products/search/route.ts
@@ -3,8 +3,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   const query = req.nextUrl.searchParams.get('query') || '';
+  console.log(req);
 
-  const products = await prisma.products.findMany({
+  const products = await prisma.product.findMany({
     where: {
       name: {
         contains: query,
--- services/products.ts
+++ services/products.ts
@@ -1,8 +1,15 @@
+// import axios from 'axios';
 import { ApiRoutes } from './constatnts';
 import { axiosInstance } from './instance';
 import { Product } from '@prisma/client';
 
-export const search = async (query: string): Promise<Product[]> => {
-  return (await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, { params: { query } }))
-    .data;
+export const search = async (query: string) => {
+  console.log(query);
+  debugger;
+  // axios.get
+  const { data } = await axiosInstance.get(ApiRoutes.SEARCH_PRODUCTS, {
+    params: { query },
+  });
+
+  return data;
 };
