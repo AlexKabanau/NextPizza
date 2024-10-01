dafb31760e30458037c6655c2e36d24b8ad8f808
Alexandr Kabanau <azraill1989@gmail.com>
2024-10-01 15:40:45 +0300

    feat: fix render 7.34

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import { Container, Title, TopBar, Filters, ProductsGroupList } from '@/components/shared';
+import { prisma } from '@/prisma/PrismaClient';
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      product: {
+        include: {
+          items: true,
+          ingredients: true,
+        },
+      },
+    },
+  });
+
+  console.log(categories);
+
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
-      <TopBar />
+      <TopBar categories={categories.filter((category) => category.product.length > 0)} />
       <Container className="mt-10 pb-14">
         <div className="flex gap-[80px]">
           {/* фильтрация */}
@@ -16,7 +30,18 @@ export default function Home() {
           {/* список товаров */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
+              {categories.map(
+                (category) =>
+                  category.product.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      items={category.product}
+                      categoryId={category.id}
+                    />
+                  ),
+              )}
+              {/* <ProductsGroupList
                 title={'Пиццы'}
                 items={[
                   {
@@ -27,114 +52,10 @@ export default function Home() {
                     price: 15,
                     items: [{ price: 15 }],
                   },
-                  {
-                    id: 2,
-                    name: 'Pizza2',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF6B5C4F298D26AF9EDAE2CB73D872.avif',
-                    price: 13,
-                    items: [{ price: 13 }],
-                  },
-                  {
-                    id: 3,
-                    name: 'Pizza3',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF6E92875574239DE842FB185A5650.avif',
-                    price: 12,
-                    items: [{ price: 12 }],
-                  },
-                  {
-                    id: 4,
-                    name: 'Pizza4',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF6E94946EFC2CBE3D38379894882E.avif',
-                    price: 11,
-                    items: [{ price: 11 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
-                  {
-                    id: 5,
-                    name: 'Pizza5',
-                    imageUrl:
-                      'https://media.dodostatic.net/image/r:233x233/11EF38570B8F74A3A71E200F82DCAEB8.avif',
-                    price: 10,
-                    items: [{ price: 10 }],
-                  },
                 ]}
                 categoryId={1}
-              />
-              <ProductsGroupList
+              /> */}
+              {/* <ProductsGroupList
                 title={'Комбо'}
                 items={[
                   {
@@ -369,7 +290,7 @@ export default function Home() {
                   },
                 ]}
                 categoryId={3}
-              />
+              /> */}
             </div>
           </div>
         </div>
--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,16 +1,18 @@
 import React, { FC } from 'react';
 import { cn } from '@/lib/utils';
 import { Categories, Container, SortPopup } from './index';
+import { Category } from '@prisma/client';
 
 type Props = {
+  categories: Category[];
   className?: string;
 };
 
-export const TopBar: FC<Props> = ({ className }) => {
+export const TopBar: FC<Props> = ({ categories, className }) => {
   return (
     <div className={cn('sticky top-0 bg-white py-5 shadow-black/5 z-10', className)}>
       <Container className="flex items-center justify-between">
-        <Categories />
+        <Categories items={categories} />
         <SortPopup />
       </Container>
     </div>
