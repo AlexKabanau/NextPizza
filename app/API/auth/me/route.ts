6773b5cc68d6c952ac3a0ad42d0923744ae49394
Alexandr Kabanau <azraill1989@gmail.com>
2024-11-12 15:19:28 +0300

    fix: fix for deploy

--- app/API/auth/me/route.ts
+++ app/API/auth/me/route.ts
@@ -1,10 +1,14 @@
 import { prisma } from '@/prisma/PrismaClient';
+import { authOptions } from '@/shared/constants/authOptions';
 import { getUserSession } from '@/shared/lib/getUserSession';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export const dynamic = 'force-dynamic';
+
+export async function GET(req: any, res: any) {
   try {
-    const user = await getUserSession();
+    const user = await getServerSession(req, res, authOptions);
 
     if (!user) {
       return NextResponse.json({ message: '[USER_GET] Unauthorized' }, { status: 401 });
